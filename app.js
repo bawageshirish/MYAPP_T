c015490830689d00a7d1b42d38f7367b4583af4f
shirish Bawage <bshirish@pkglobal.com>
2021-12-28 11:35:38 +0530

    simple app

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
-require("dotenv/config");
-require("./db/connection");
-
 const express = require("express");
 const app = express();
-const User = require("./models/user");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  console.log("First Request..!!");
+  res.send("First Request..!!");
 });
 
 app.get("/users", (req, res) => {
-  User.find(function (err, users) {
-    res.send({ "All Users": users });
-  });
+  res.send({ "All Users": { name: "shirish", age: 31 } });
 });
 
-app.post("/create_user", async (req, res) => {
-  try {
-    console.log("create_user --start");
-    const myuser = new User(req.body);
-    console.log("myuser");
-    console.log(myuser);
-    await myuser.save();
-    console.log("create_user --saved");
-
-    res.send(myuser);
-    console.log("create_user --responded");
-  } catch (err) {
-    res.send(err);
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Listening to port 3000");
+app.listen(process.env.PORT || 3333, () => {
+  console.log("Listening to port 3333");
 });
